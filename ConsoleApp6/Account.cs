1715953278d8a80d9ab7110101be7d751aae3373
Joa2272277 <zuchora.joannna@gmail.com>
2022-04-22 12:20:13 +0200

    first commit

---
+++ ConsoleApp6/Account.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    public class Account
+    {
+        private static int CurrentId { get; set; } = 1;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountNumber { get; set; }
+        public string PIN { get; set; }
+
+        public Account()
+        {
+            var random = new Random();
+            Id = CurrentId;
+            CurrentId++;
+            Balance = 0;
+            AccountNumber = Id.ToString() + random.Next(10000-99000);
+
+        }
+
+        public Account(string name, string surname, DateTime dateOfBirth, string pin)
+        {
+            var random = new Random();
+            Id = CurrentId;
+            CurrentId++;
+            Name = name;
+            Surname = surname;
+            DateOfBirth = dateOfBirth;
+            Balance = 0;
+            AccountNumber = Id.ToString() + random.Next(10000, 99000);
+            PIN = pin;
+        }
+
+        ~Account()
+        {
+            Console.WriteLine("Wywolanie destrutora. Usuwanie uzytkowanika " + Name + Id);
+        }
+
+        public string Introduce() 
+        {
+            return $"My name is {Name}, my surname is " + Surname + ", " 
+            + "my age is " + (DateTime.Now.Year - DateOfBirth.Year) + " moje Id to " + Id + " stan konta to " + Balance + " mój numer konta to " + AccountNumber; 
+        }
+
+        public void DepositMoney()
+        {
+            Console.WriteLine("Podaj kwote do wpłacenia ");
+            Balance += Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Nowy stan konta {Balance}");
+        }
+
+        public void WithdrawMoney()
+        {
+            Console.WriteLine("Podaj kwote do wypłacenia ");
+            var valueToWithdraw = Int32.Parse(Console.ReadLine());
+            if (valueToWithdraw > Balance)
+            {
+                Console.WriteLine("Nie mozesz wyplacic kwoty ");
+                return;
+            }
+            Balance -= valueToWithdraw;
+            Console.WriteLine($"Nowy stan konta {Balance}");
+        }
+
+        public static Account CreateAccount()
+        {
+            Console.WriteLine("Podaj Imie");
+            var name = Console.ReadLine();
+            Console.WriteLine("Podaj nazwisko ");
+            var surname = Console.ReadLine();
+            Console.WriteLine("Podaj date urodzenia ");
+            var dateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            var PIN = string.Empty;
+            var IsValid = false;
+            while (!IsValid)
+            {
+                Console.WriteLine("Podaj PIN");
+                PIN = Console.ReadLine();
+                IsValid = VerifyPIN(PIN);
+            }
+
+            var account = new Account(name, surname, dateOfBirth, PIN);
+            return account;
+        }
+        
+        static bool VerifyPIN(string pin)
+        {
+            if (pin.Length != 4)
+            {
+                Console.WriteLine("Błędny PIN: Podaj PIN który ma 4 cyfry ");
+                return false;
+            }
+            foreach (var znak in pin)
+            {
+                if (!char.IsDigit(znak))
+                {
+                    Console.WriteLine("Błędny PIN: Podaj PIN który zawiera tylko cyfry ");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
+
+
+   
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,81 @@
+﻿using ConsoleApp6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static List<Account> Accounts { get; set; }
+
+        static void Main(string[] args)
+        {
+            Accounts = new List<Account>();
+
+            string opcja;
+            do
+            {
+                Console.WriteLine("Wybierz opcje: 0 - zakoncz program, 1 - Utworz konto, 2 - wyplac pieniadze, 3 - wpłać pieniądze, 4 - usuń konto");
+                opcja = Console.ReadLine();
+
+                if (opcja == "1")
+                {
+                    var account = Account.CreateAccount();
+                    Accounts.Add(account);
+                    Console.WriteLine("Uzytkownicy na liscie to: ");
+                    foreach (var user in Accounts)
+                    {
+                        Console.WriteLine(user.Introduce());
+                    }
+                }
+                if (opcja == "2")
+                {
+                    Console.WriteLine("Podaj swoje ID");
+                    var id = Int32.Parse(Console.ReadLine());
+                    var user = Accounts.FirstOrDefault(x => x.Id == id);
+                    if (user == null)
+                    {
+                        Console.WriteLine("Uzytkownik o takim ID nie istnieje ");
+                        return;
+                    }
+                    user.WithdrawMoney();
+                }
+                if (opcja == "3")
+                {
+                    Console.WriteLine("Podaj swoje ID");
+                    var id = Int32.Parse(Console.ReadLine());
+                    var user = Accounts.FirstOrDefault(x => x.Id == id);
+                    if (user == null)
+                    {
+                        Console.WriteLine("Uzytkownik o takim ID nie istnieje ");
+                        return;
+                    }
+                    user.DepositMoney();
+                }
+                if (opcja == "4")
+                {
+                    Console.WriteLine("podaj Id uzytkowanika do usuniecia: ");
+                    var doUsuniecia = Convert.ToInt32(Console.ReadLine());
+
+                    DeleteAccount(doUsuniecia);
+                }
+
+            }
+            while (opcja != "0");
+        }
+
+        static void DeleteAccount(int id)
+        {
+            for (int i = 0; i < Accounts.Count; i++)
+            {
+                if (Accounts[i].Id == id)
+                {
+                    Accounts.RemoveAt(i);
+                    Console.WriteLine("usunieto uzytkownika o Id " + id);
+                    return;
+                }
+            }
+        }
+    }
+}
